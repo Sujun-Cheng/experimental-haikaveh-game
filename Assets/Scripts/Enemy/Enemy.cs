1f3c9cce90fea9ce6ab0bcfe0a01d155ea25504d
Zhe Dang <zdang31@gatech.edu>
2025-10-19 06:53:26 -0300

    enemy attack/gethit/die interaction

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+    private Animator anim;
+    private bool isDead = false;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void TakeDamage(float damage, Vector3 hitPosition)
+    {
+        if (isDead) return;
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            if (anim != null)
+            {
+                anim.SetTrigger("hit"); 
+            }
+        }
+    }
+
+    void Die()
+    {
+        isDead = true; 
+
+        if (anim != null)
+        {
+            anim.SetTrigger("death"); 
+        }
+    }
+}
