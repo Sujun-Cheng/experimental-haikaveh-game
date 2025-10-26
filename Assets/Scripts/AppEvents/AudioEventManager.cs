146f40fae7a021120eccf56c7485ab755756705c
Qianqian Shao <110926331+qqs377@users.noreply.github.com>
2025-10-26 14:24:03 -0400

    clean scene

--- Assets/Scripts/AppEvents/AudioEventManager.cs
+++ Assets/Scripts/AppEvents/AudioEventManager.cs
@@ -8,7 +8,7 @@ public class AudioEventManager : MonoBehaviour
 
     public EventSound3D eventSound3DPrefab;
 
-    public AudioClip[] minionJabberAudio = null;
+
     public AudioClip[] boxAudio = null;
     public AudioClip playerLandsAudio;
     public AudioClip explosionAudio;
@@ -16,10 +16,6 @@ public class AudioEventManager : MonoBehaviour
     public AudioClip bombBounceAudio;
     public AudioClip jumpAudio;
     public AudioClip gruntAudio;
-    public AudioClip minionDeathAudio;
-    public AudioClip minionOuchAudio;
-    public AudioClip minionSpawnAudio;
-    public AudioClip[] minionFootstepAudio;
     public AudioClip punchAudio;
     public AudioClip[] footStepAudio;
 
@@ -27,9 +23,7 @@ public class AudioEventManager : MonoBehaviour
 
     private UnityAction<Vector3, float> playerLandsEventListener;
 
-    private UnityAction<Vector3, float> minionLandsEventListener;
 
-    private UnityAction<Vector3> minionJabberEventListener;
 
     private UnityAction<Vector3> explosionEventListener;
 
@@ -39,13 +33,6 @@ public class AudioEventManager : MonoBehaviour
 
     private UnityAction<GameObject> deathEventListener;
 
-    private UnityAction<Vector3,MinionScript> minionDeathEventListener;
-
-    private UnityAction<MinionScript> minionSpawnEventListener;
-
-    private UnityAction<Vector3> minionFootstepEventListener;
-
-    private UnityAction<Vector3> minionOuchEventListener;
 
     private UnityAction<Vector3> playerFootstepEventListener;
 
@@ -56,11 +43,11 @@ public class AudioEventManager : MonoBehaviour
 
         playerLandsEventListener = new UnityAction<Vector3, float>(playerLandsEventHandler);
 
-        minionLandsEventListener = new UnityAction<Vector3, float>(minionLandsEventHandler);
+    
 
         explosionEventListener = new UnityAction<Vector3>(explosionEventHandler);
 
-        minionJabberEventListener = new UnityAction<Vector3>(minionJabberEventHandler);
+   
 
         bombBounceEventListener = new UnityAction<Vector3>(bombBounceEventHandler);
 
@@ -68,13 +55,7 @@ public class AudioEventManager : MonoBehaviour
 
         deathEventListener = new UnityAction<GameObject>(deathEventHandler);
 
-        minionDeathEventListener = new UnityAction<Vector3,MinionScript>(minionDeathEventHandler);
-
-        minionSpawnEventListener = new UnityAction<MinionScript>(minionSpawnEventHandler);
-
-        minionFootstepEventListener = new UnityAction<Vector3>(minionFootstepEventHandler);
-
-        minionOuchEventListener = new UnityAction<Vector3>(minionOuchEventHandler);
+ 
 
         playerFootstepEventListener = new UnityAction<Vector3>(playerFootstepsEventHandler);
     }
@@ -94,16 +75,12 @@ public class AudioEventManager : MonoBehaviour
 
         EventManager.StartListening<BoxCollisionEvent, Vector3,float>(boxCollisionEventListener);
         EventManager.StartListening<PlayerLandsEvent, Vector3, float>(playerLandsEventListener);
-        EventManager.StartListening<MinionLandsEvent, Vector3, float>(minionLandsEventListener);
-        EventManager.StartListening<MinionJabberEvent, Vector3>(minionJabberEventListener);
+ 
         EventManager.StartListening<ExplosionEvent, Vector3>(explosionEventListener);
         EventManager.StartListening<BombBounceEvent, Vector3>(bombBounceEventListener);
         EventManager.StartListening<JumpEvent, Vector3>(jumpEventListener);
         EventManager.StartListening<DeathEvent, GameObject>(deathEventListener);
-        EventManager.StartListening<MinionDeathEvent, Vector3, MinionScript>(minionDeathEventListener);
-        EventManager.StartListening<MinionSpawnEvent, MinionScript>(minionSpawnEventListener);
-        EventManager.StartListening<MinionFootstepEvent, Vector3>(minionFootstepEventListener);
-        EventManager.StartListening<MinionOuchEvent, Vector3>(minionOuchEventListener);
+ 
         EventManager.StartListening<PlayerFootstepsEvent, Vector3>(playerFootstepEventListener);
 
     }
@@ -113,16 +90,12 @@ public class AudioEventManager : MonoBehaviour
 
         EventManager.StopListening<BoxCollisionEvent, Vector3,float>(boxCollisionEventListener);
         EventManager.StopListening<PlayerLandsEvent, Vector3, float>(playerLandsEventListener);
-        EventManager.StopListening<MinionLandsEvent, Vector3, float>(minionLandsEventListener);
-        EventManager.StopListening<MinionJabberEvent, Vector3>(minionJabberEventListener);
+      
         EventManager.StopListening<ExplosionEvent, Vector3>(explosionEventListener);
         EventManager.StopListening<BombBounceEvent, Vector3>(bombBounceEventListener);
         EventManager.StopListening<JumpEvent, Vector3>(jumpEventListener);
         EventManager.StopListening<DeathEvent, GameObject>(deathEventListener);
-        EventManager.StopListening<MinionDeathEvent, Vector3, MinionScript>(minionDeathEventListener);
-        EventManager.StopListening<MinionSpawnEvent, MinionScript>(minionSpawnEventListener);
-        EventManager.StopListening<MinionFootstepEvent, Vector3>(minionFootstepEventListener);
-        EventManager.StopListening<MinionOuchEvent, Vector3>(minionOuchEventListener);
+
         EventManager.StopListening<PlayerFootstepsEvent, Vector3>(playerFootstepEventListener);
     }
 
@@ -184,61 +157,8 @@ public class AudioEventManager : MonoBehaviour
         }
     }
 
-    void minionLandsEventHandler(Vector3 worldPos, float collisionMagnitude)
-    {
-        //AudioSource.PlayClipAtPoint(this.explosionAudio, worldPos, 1f);
+ 
 
-        if (eventSound3DPrefab)
-        {
-            if (collisionMagnitude > 300f)
-            {
-
-                EventSound3D snd = Instantiate(eventSound3DPrefab, worldPos, Quaternion.identity, null);
-
-                snd.audioSrc.clip = this.punchAudio;
-
-                snd.audioSrc.minDistance = 5f;
-                snd.audioSrc.maxDistance = 100f;
-
-                snd.audioSrc.Play();
-
-                if (collisionMagnitude > 500f)
-                {
-
-                    EventSound3D snd2 = Instantiate(eventSound3DPrefab, worldPos, Quaternion.identity, null);
-
-                    snd2.audioSrc.clip = this.minionOuchAudio;
-
-                    snd2.audioSrc.minDistance = 5f;
-                    snd2.audioSrc.maxDistance = 100f;
-
-                    snd2.audioSrc.Play();
-                }
-            }
-
-
-        }
-    }
-
-    void minionJabberEventHandler(Vector3 worldPos)
-    {
-        //AudioSource.PlayClipAtPoint(this.explosionAudio, worldPos, 1f);
-
-        if (eventSound3DPrefab)
-        {
-
-            EventSound3D snd = Instantiate(eventSound3DPrefab, worldPos, Quaternion.identity, null);
-
-            snd.gameObject.AddComponent<MinionAudioCancelOnDeath>();
-
-            snd.audioSrc.clip = this.minionJabberAudio[Random.Range(0, this.minionJabberAudio.Length)];
-
-            snd.audioSrc.minDistance = 5f;
-            snd.audioSrc.maxDistance = 100f;
-
-            snd.audioSrc.Play();
-        }
-    }
 
     void explosionEventHandler(Vector3 worldPos)
     {
@@ -312,94 +232,7 @@ public class AudioEventManager : MonoBehaviour
         }
     }
 
-    void minionDeathEventHandler(Vector3 pos, MinionScript ms)
-    {
-  
-        if (minionDeathAudio)
-        {
 
-            EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
-
-            snd.audioSrc.clip = this.minionDeathAudio;
-
-            snd.audioSrc.minDistance = 5f;
-            snd.audioSrc.maxDistance = 100f;
-
-            snd.audioSrc.Play();
-        }
-
-    }
-
-
-    void minionOuchEventHandler(Vector3 pos)
-    {
-
-
-        if (punchAudio)
-        {
-
-            EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
-
-            snd.audioSrc.clip = this.punchAudio;
-
-            snd.audioSrc.minDistance = 5f;
-            snd.audioSrc.maxDistance = 100f;
-
-            snd.audioSrc.Play();
-        }
-
-  
-        if (minionOuchAudio)
-        {
-
-            EventSound3D snd2 = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
-
-            snd2.audioSrc.clip = this.minionOuchAudio;
-
-            snd2.audioSrc.minDistance = 5f;
-            snd2.audioSrc.maxDistance = 100f;
-
-            snd2.audioSrc.Play();
-        }
-    }
-
-
-    void minionSpawnEventHandler(MinionScript minion) {
-
-        if (minionSpawnAudio)
-        {
-
-            EventSound3D snd = Instantiate(eventSound3DPrefab, minion.transform.position, Quaternion.identity, null);
-
-            snd.audioSrc.clip = this.minionSpawnAudio;
-
-            snd.audioSrc.minDistance = 5f;
-            snd.audioSrc.maxDistance = 100f;
-
-            snd.audioSrc.Play();
-        }
-
-
-    }
-
-
-    void minionFootstepEventHandler(Vector3 pos) {
-
-        if (minionSpawnAudio)
-        {
-
-            EventSound3D snd = Instantiate(eventSound3DPrefab, pos, Quaternion.identity, null);
-
-            snd.audioSrc.clip = this.minionFootstepAudio[Random.Range(0, minionFootstepAudio.Length)];
-
-            snd.audioSrc.minDistance = 5f;
-            snd.audioSrc.maxDistance = 100f;
-
-            snd.audioSrc.Play();
-        }
-
-
-    }
 
     void playerFootstepsEventHandler(Vector3 pos)
     {
