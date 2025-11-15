e13c10869ca5664417b9df144658a1d3722f1dbf
Sujun-Cheng <sujuncheng2001@gmail.com>
2025-11-14 21:26:39 -0500

    set alhaitham to become ai follower only after kaveh encounters him

---
+++ Assets/Scripts/AddPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AddPlayer : MonoBehaviour
+{
+    [SerializeField] private CharacterManager characterManager;
+    public GameObject playerCompanion;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter(Collider collider)
+    {
+        if (characterManager != null && collider.gameObject!= playerCompanion)
+        {
+            characterManager.AddCompanion(playerCompanion);
+
+        }
+    }
+
+}
