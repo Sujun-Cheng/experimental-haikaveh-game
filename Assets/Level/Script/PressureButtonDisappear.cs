b671c48e40fd803539ea5b094cc4e75434a9e90f
Sujun-Cheng <sujuncheng2001@gmail.com>
2025-11-23 03:57:07 -0500

    added more objectives and dialogue

---
+++ Assets/Level/Script/PressureButtonDisappear.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class PressureButtonDisappear : MonoBehaviour
+{
+    public GameObject[] items;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool isDown = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isDown)
+        {
+            isDown = true;
+            StartCoroutine(FallAway());
+           
+            
+            
+        }
+    }
+    private IEnumerator FallAway()
+    {
+        foreach (var item in items)
+        {
+            item.SetActive(false);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+}
--- Assets/Scripts/UI/ObjectiveUI.cs
+++ Assets/Scripts/UI/ObjectiveUI.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
