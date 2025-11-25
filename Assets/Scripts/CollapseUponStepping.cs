1921b8c2982269b472ce532f3ef054c158c5a2d7
Sujun-Cheng <sujuncheng2001@gmail.com>
2025-11-24 21:56:57 -0500

    added sound effect for bridge

--- Assets/Scripts/CollapseUponStepping.cs
+++ Assets/Scripts/CollapseUponStepping.cs
@@ -12,7 +12,13 @@ public class CollapseUponStepping : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         rb.isKinematic = false;
-        rb.useGravity = true; 
+        rb.useGravity = true;
+        if (collision.impulse.magnitude > 0.25f)
+        {
+            //we'll just use the first contact point for simplicity
+            EventManager.TriggerEvent<BoxCollisionEvent, Vector3, float>(collision.contacts[0].point, collision.impulse.magnitude);
+
+        }
     }
 
 }
